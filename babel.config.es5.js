2e13306a0675b502b6859e5a0bd39b8dcfe042a9
Sam McTaggart <sam.mctaggart@gmail.com>
2019-03-05 16:29:51 +0000

    Create es5 babel config
    This is the same as our previous default config with the exception that
    our target is now `esmodules: false` (i.e. browsers that don't support
    `type=module`)
    
    We have also introduced a new webpack loader that uses this config so
    that we can apply it in a future commit to our legacy bundles.

---
+++ babel.config.es5.js
@@ -0,0 +1,51 @@
+module.exports = (api) => {
+  const presets = [
+    ['@babel/preset-env', {
+      modules: false,
+      useBuiltIns: 'usage',
+      targets: {
+        esmodules: false,
+      },
+      exclude: ['transform-regenerator', 'transform-async-to-generator'],
+    }],
+    '@babel/preset-react',
+  ];
+
+  const plugins = [
+    '@babel/plugin-proposal-object-rest-spread',
+    '@babel/plugin-syntax-dynamic-import',
+    'lodash',
+    [
+      '@babel/plugin-proposal-class-properties',
+      {
+        spec: true,
+      },
+    ],
+    [
+      'module:fast-async',
+      {
+        spec: true,
+      },
+    ],
+  ];
+
+  if (api.env('test')) {
+    plugins.push(...[
+      '@babel/plugin-transform-modules-commonjs',
+      'dynamic-import-node',
+    ]);
+  }
+
+  if (api.env('production')) {
+    plugins.push(
+      ['transform-react-remove-prop-types', {
+        ignoreFilenames: ['node_modules'],
+      }],
+    );
+  }
+
+  return {
+    presets,
+    plugins,
+  };
+};
