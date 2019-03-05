8a95eb6d5fa553268bd616a7bbf37d95b91c57a4
Sam McTaggart <sam.mctaggart@gmail.com>
2019-03-05 16:40:13 +0000

    Development Legacy bundle
    The only changes here are that we are using our `sharedLegacy` config
    that we introduced in the previous commit and have changed the name of
    the manifest to be `manifest-legacy.json` (as opposed to the default
    `manifest.json`)

--- workspaces/webpack-config/index.js
+++ workspaces/webpack-config/index.js
@@ -2,10 +2,12 @@ const development = require('./src/development');
 const production = require('./src/production');
 const test = require('./src/test');
 const hypernova = require('./src/hypernova');
+const developmentLegacy = require('./src/development_legacy');
 
 module.exports = {
   development,
   test,
   production,
   hypernova,
+  developmentLegacy,
 };
--- workspaces/webpack-config/src/loaders/index.js
+++ workspaces/webpack-config/src/loaders/index.js
@@ -1,7 +1,9 @@
 const sass = require('./sass');
+const noSassLoader = require('../server_side_loaders/no_sass');
 const file = require('./file');
 const svg = require('./svg');
 const babel = require('./babel');
+const babelES5 = require('./babel.es5');
 const babelNode = require('../server_side_loaders/babel.node');
 
 module.exports.base = {
@@ -9,6 +11,13 @@ module.exports.base = {
   file,
   svg,
   babel,
+};
+
+module.exports.legacy = {
+  noSassLoader,
+  file,
+  svg,
+  babelES5,
 };
 
 module.exports.hypernova = {
