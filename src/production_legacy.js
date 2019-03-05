34d07c3361840e706b0bf6b917411e53f2255ad8
Sam McTaggart <sam.mctaggart@gmail.com>
2019-03-05 16:40:24 +0000

    Production legacy bundle
    This modifies our production config to
    
    1. Use shared-legacy
    2. Rename the manifest to `manifest-legacy.json`
    3. Removed `MiniCssExtractPlugin` (since we ignore sass and css in our
    legacy bundles)

---
+++ src/production_legacy.js
@@ -0,0 +1,45 @@
+const WebpackAssetsManifest = require('webpack-assets-manifest');
+const TerserPlugin = require('terser-webpack-plugin');
+const HardSourceWebpackPlugin = require('hard-source-webpack-plugin');
+const { HashedModuleIdsPlugin, NamedChunksPlugin } = require('webpack');
+const shared = require('./shared_legacy');
+
+module.exports = {
+  ...shared,
+  name: 'production',
+  devtool: 'none',
+  optimization: {
+    ...shared.optimization,
+    minimize: true,
+    minimizer: [new TerserPlugin({
+      parallel: true,
+      cache: true,
+      sourceMap: false,
+      terserOptions: {
+        compress: false,
+        ecma: 5,
+        warnings: false,
+        safari10: true,
+        ie8: false,
+        output: {
+          ascii_only: true,
+        },
+      },
+    })],
+  },
+  plugins: [
+    ...shared.plugins,
+    new HardSourceWebpackPlugin(),
+    new HardSourceWebpackPlugin.ExcludeModulePlugin([{
+      test: /mini-css-extract-plugin[\\/]dist[\\/]loader/,
+    }]),
+    new HashedModuleIdsPlugin({}),
+    new NamedChunksPlugin(),
+    new WebpackAssetsManifest({
+      output: 'manifest-legacy.json',
+      entrypoints: true,
+      writeToDisk: true,
+      publicPath: shared.output.publicPath,
+    }),
+  ],
+};
