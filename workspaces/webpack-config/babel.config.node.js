29904babd60db26f4a418b87d3791ac47642279c
Sam McTaggart <sam.mctaggart@gmail.com>
2019-03-05 16:30:02 +0000

    Create node babel config
    This updates the targets to be `node` which should allow us to have a
    more performant server side bundle. `node: 'current'` means that babel
    will target the current version of node that is available on the
    machine.
    
    Since our version of node supports `async await` and
    `object-rest-spread` I've removed those plugins from this config as
    well.

---
+++ workspaces/webpack-config/babel.config.node.js
@@ -0,0 +1,44 @@
+module.exports = (api) => {
+  const presets = [
+    ['@babel/preset-env', {
+      modules: false,
+      useBuiltIns: 'usage',
+      targets: {
+        node: 'current',
+      },
+      exclude: ['transform-regenerator', 'transform-async-to-generator'],
+    }],
+    '@babel/preset-react',
+  ];
+
+  const plugins = [
+    '@babel/plugin-syntax-dynamic-import',
+    'lodash',
+    [
+      '@babel/plugin-proposal-class-properties',
+      {
+        spec: true,
+      },
+    ],
+  ];
+
+  if (api.env('test')) {
+    plugins.push(...[
+      '@babel/plugin-transform-modules-commonjs',
+      'dynamic-import-node',
+    ]);
+  }
+
+  if (api.env('production')) {
+    plugins.push(
+      ['transform-react-remove-prop-types', {
+        ignoreFilenames: ['node_modules'],
+      }],
+    );
+  }
+
+  return {
+    presets,
+    plugins,
+  };
+};
