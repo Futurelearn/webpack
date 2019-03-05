bb0f0752e8e06ba51ce4120524fc90b43576d5c2
Sam McTaggart <sam.mctaggart@gmail.com>
2019-03-05 16:39:30 +0000

    Introduce shared config for legacy bundles
    We need a different base config for our Webpack config that's
    responsible for producing our legacy bundles.
    
    The main changes are:
    
    * The entries need to be looking for our legacy entries (i.e.
    `app/javascript-legacy`)
    * Our resolved paths need to factor in `app/javascript-legacy`
    * We need to use the babel config targeting ES5
    * We need to suffix the outputs with `es5` to make it easy to tell which
    bundle we are loading in the browser.
    
    This commit adds theses changes.

---
+++ workspaces/webpack-config/src/shared_legacy.js
@@ -0,0 +1,26 @@
+const config = require('./config');
+const loaders = require('./loaders');
+const { mode, plugins, optimization } = require('./shared');
+
+module.exports = {
+  entry: config.legacyEntries,
+  resolve: {
+    extensions: config.extensions,
+    modules: [...config.legacyResolvedPaths, 'node_modules'],
+    alias: {
+      tribute: 'tributejs/dist/tribute.min.js',
+    },
+  },
+  module: {
+    rules: Object.values(loaders.legacy),
+  },
+  mode,
+  plugins,
+  optimization,
+  output: {
+    path: config.output.path,
+    publicPath: config.output.publicPath,
+    filename: `[name]-[chunkhash]-${config.assetsVersion}.es5.js`,
+    chunkFilename: `[name]-[chunkhash]-${config.assetsVersion}.es5.js`,
+  },
+};
