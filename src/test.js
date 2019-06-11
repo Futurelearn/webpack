e192b844d0e2510d802ad700f39dd6067989b713
James Hunt <ohthatjames@gmail.com>
2019-06-11 16:56:08 +0100

    Rename

--- src/test.js
+++ src/test.js
@@ -14,7 +14,7 @@ module.exports = {
     }),
     new DefinePlugin({
       __REACT_DEVTOOLS_GLOBAL_HOOK__: '({ isDisabled: true })',
-      DISABLE_SCROLLING: true,
+      __FL_DISABLE_IN_TEST__: true,
     }),
   ],
 };
