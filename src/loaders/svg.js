cbe5257897ad8e6b4433e137c43c8c56276dd3e8
Seb Jacobs <me@sebjacobs.com>
2019-07-16 16:54:50 +0100

    Configure cache-loader for svgs

--- src/loaders/svg.js
+++ src/loaders/svg.js
@@ -1,6 +1,17 @@
+const { join, resolve } = require('path');
+const config = require('../config');
+
+const cacheLoaderCachePath = join(config.cachePath, 'svg-cache-loader');
+
 module.exports = {
   test: /shared\/svg_icons\/.+html/,
   use: [
+    {
+      loader: 'cache-loader',
+      options: {
+        cacheDirectory: cacheLoaderCachePath,
+      },
+    },
     {
       loader: '@svgr/webpack',
       options: {
