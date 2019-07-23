69ce54c5677d147a4ffbc5a3871e33d8041087af
juweez + wannabefro <pairing+juweez+wannabefro@futurelearn.com>
2019-07-23 11:14:46 +0100

    how did this ever work

--- src/loaders/css_module_options.js
+++ src/loaders/css_module_options.js
@@ -2,26 +2,27 @@ const loaderUtils = require('loader-utils');
 const { posix } = require('path');
 
 const CSS_MODULE_OPTIONS = {
-  modules: true,
-  getLocalIdent(context, localIdentName, localName, options) {
-    const fileNameOrFolder = context.resourcePath.match(/index\.module\.(css|scss|sass)$/)
-      ? '[folder]'
-      : '[name]';
+  modules: {
+    getLocalIdent(context, localIdentName, localName, options) {
+      const fileNameOrFolder = context.resourcePath.match(/index\.module\.(css|scss|sass)$/)
+        ? '[folder]'
+        : '[name]';
 
-    const hash = loaderUtils.getHashDigest(
-      posix.relative(context.rootContext, context.resourcePath) + localName,
-      'md5',
-      'base64',
-      5,
-    );
+      const hash = loaderUtils.getHashDigest(
+        posix.relative(context.rootContext, context.resourcePath) + localName,
+        'md5',
+        'base64',
+        5,
+      );
 
-    const className = loaderUtils.interpolateName(
-      context,
-      `${fileNameOrFolder}-${localName}_${hash}`,
-      options,
-    );
+      const className = loaderUtils.interpolateName(
+        context,
+        `${fileNameOrFolder}-${localName}_${hash}`,
+        options,
+      );
 
-    return className.replace('.module', '');
+      return className.replace('.module', '');
+    },
   },
   localsConvention: 'asIs',
   sourceMap: process.env.NODE_ENV !== 'production',
