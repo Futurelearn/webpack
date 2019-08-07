c98ae240d64f5d399dde2475e07ee1b5a7d3d24c
Dan Williams <dan.williams@futurelearn.com>
2019-08-07 14:13:47 +0100

    Maybe try different regex for the whitelist and the null loader?

--- src/server_side_loaders/client_side_only_packages.js
+++ src/server_side_loaders/client_side_only_packages.js
@@ -1,3 +1,3 @@
-const CLIENT_SIDE_ONLY_PACKAGES = /.*node_modules.*\b(c3|d3|d3v4|react-select|tribute)\b/i;
+const CLIENT_SIDE_ONLY_PACKAGES = /\b(c3|d3|d3v4|react-select|tribute)\b/i;
 
 module.exports = CLIENT_SIDE_ONLY_PACKAGES;
--- src/server_side_loaders/null_loader.js
+++ src/server_side_loaders/null_loader.js
@@ -1,6 +1,6 @@
 const CLIENT_SIDE_ONLY_PACKAGES = require('./client_side_only_packages');
 
 module.exports = {
-  test: CLIENT_SIDE_ONLY_PACKAGES,
+  test: /.*node_modules.*\b(c3|d3|d3v4|react-select|tribute)\b/i,
   use: 'null-loader',
 };
