051683b9ae941cbf79af604e1f18f55593a95b02
Joseph <joseph.morrow@futurelearn.com>
2019-08-09 12:59:29 +0100

    DO NOT MERGE: Only use sass-resources-loader for SCSS files
    https://trello.com/c/KkoxiW3H
    
    This loader made a bunch of SCSS mixins and variables available by
    default to any of our SCSS files or CSS modules.
    
    We'd like to explicitly import dependencies rather than having them be
    available globally. As a first step, we are no longer making these
    global mixins and variables available to CSS modules by default.
    
    To make sure our CSS modules continue to work after this commit is
    merged, we've updated our CSS modules to explicitly import their
    dependenies here: https://github.com/Futurelearn/futurelearn/pull/2756

--- src/loaders/sass.js
+++ src/loaders/sass.js
@@ -1,9 +1,20 @@
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const { resolve } = require('path');
 const DEFAULT_STYLE_LOADERS = require('./default_style_loaders');
 
 const use = [
   'css-loader',
   ...DEFAULT_STYLE_LOADERS,
+  {
+    loader: 'sass-resources-loader',
+    options: {
+      resources: [
+        resolve('app', './assets/stylesheets/application/1-tools/_all.scss'),
+        resolve('app', './assets/stylesheets/application/2-brand/_all.scss'),
+        resolve('app', './assets/stylesheets/application/base/_all.scss'),
+      ],
+    },
+  },
 ];
 
 if (process.env.NODE_ENV !== 'production') {
