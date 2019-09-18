7e58d064946b18ec53ecdcb9dc4045472e71ca77
Tom Marshall <tom.marshall@futurelearn.com>
2019-09-18 13:04:45 +0100

    Remove unused test config
    Our main app now injects the `window.__FL_DISABLE_IN_TEST__` global and
    disables React dev-tools via another mechanism. We did this so we could
    build a single set of assets for both `test` and `development`
    environments. As such, this `test` config is no longer used by our main
    app and can be deleted.

--- workspaces/webpack-config/index.js
+++ workspaces/webpack-config/index.js
@@ -1,13 +1,11 @@
 const development = require('./src/development');
 const production = require('./src/production');
-const test = require('./src/test');
 const hypernova = require('./src/hypernova');
 const developmentLegacy = require('./src/development_legacy');
 const productionLegacy = require('./src/production_legacy');
 
 module.exports = {
   development,
-  test,
   production,
   hypernova,
   developmentLegacy,
