8fca07d8ea475d5b326a171774e6552b5ee1dfd6
Tom Marshall <tom.marshall@futurelearn.com>
2019-09-24 14:37:07 +0100

    Remove safari-nomodule.js test
    We added this in 359141c to ensure `safari-nomodule.js` was loaded as a
    static file, instead of being processed as a JavaScript file. We've
    since changed how we're including that polyfill, such that it is no
    longer processed by webpack at all. As such, this reverts that commit.

--- workspaces/webpack-config/src/loaders/file.js
+++ workspaces/webpack-config/src/loaders/file.js
@@ -1,8 +1,5 @@
 module.exports = {
-  test: [
-    /\.(jpg|jpeg|png|gif|tiff|ico|svg|eot|otf|ttf|woff|woff2)$/i,
-    /app\/javascript\/vendor\/safari-nomodule.js/,
-  ],
+  test: /\.(jpg|jpeg|png|gif|tiff|ico|svg|eot|otf|ttf|woff|woff2)$/i,
   use: [
     {
       loader: 'file-loader',
